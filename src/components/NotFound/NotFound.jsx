3b1dd00c448cf03064564f62201c2118339c7ebf
collins njau <collinsnjau39@gmail.com>
2018-11-15 18:46:09 +0300

    Add custom error page

---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class NotFound extends Component {
+    render(){
+        return(
+            <h1>OOOPS YOU SHOULD NOT BE HERE</h1>
+        )
+    }
+
+};
+export default NotFound
--- src/components/Router/Router.jsx
+++ src/components/Router/Router.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {NavLink, Switch, Route } from 'react-router-dom'
 import App from '../../views/App/App'
 import About from '../../views/About/About'
+import NotFound from '../../components/NotFound/NotFound'
 
 const Router = () => (
     <Switch>
         <Route exact path="/" component={App}></Route>
         <Route exact path="/about" component={About}></Route>
+        <Route path="*" component={NotFound}></Route>
     </Switch>
 )
 
